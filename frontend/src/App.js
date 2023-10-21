41544def21485d622d55231b56a76e7c6c216ef8
Francesco  OTERI <f.oteri@instadeep.com>
2023-10-21 16:55:35 +0200

    feature: add submit logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,15 @@ function App() {
     setFiles(prevFiles => [...prevFiles, ...acceptedFiles]);
   };
 
+  const handleSubmit = () => {
+    // Handle submit action here
+    console.log('Files submitted:', files);
+  };
+
   return (
     <div>
       <FileUpload onFileUpload={handleFileUpload} acceptedExtensions={['fasta', 'a3m']} />
+      <button onClick={handleSubmit}>Submit</button>
       <FileList files={files} />
     </div>
   );
