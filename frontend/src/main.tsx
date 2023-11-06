a357da75bb64b7857590430e62a840d047a79ddf
Francesco Oteri <francesco.oteri@gmail.com>
2023-11-06 23:39:51 +0100

    feature: vite working. Yuppie!

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'dotenv/config';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env.MINIO_ROOT_USER': JSON.stringify(process.env.MINIO_ROOT_USER),
+    'process.env.MINIO_ROOT_PASSWORD': JSON.stringify(process.env.MINIO_ROOT_PASSWORD),
+    'process.env.MINIO_ACCESS_KEY': JSON.stringify(process.env.MINIO_ACCESS_KEY),
+    'process.env.MINIO_SECRET_KEY': JSON.stringify(process.env.MINIO_SECRET_KEY),
+    'process.env.MINIO_BUCKET_NAME': JSON.stringify(process.env.MINIO_BUCKET_NAME),
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  }
+});
