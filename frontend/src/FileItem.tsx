168e27cb5aa534781639e5829b9229088f0ee271
Francesco Oteri <francesco.oteri@gmail.com>
2023-11-26 21:39:24 +0100

    refactor: FileItem moved to its own file

---
+++ frontend/src/FileItem.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { uploadFileToS3 } from './file_utils';
+import React, { useEffect, useImperativeHandle, useState } from 'react';
+
+
+const StyledFileItem = styled.li`
+    margin: 10px 0;
+`;
+
+export interface FileItemHandle {
+    handleSubmitEvent: () => void;
+  }
+
+interface FileItemProps {
+file: File;
+}
+
+export const FileItem = React.forwardRef<FileItemHandle, FileItemProps>(({ file }, ref) => {
+    const [fileUrl, setFileUrl] = useState<string|null>(null)
+    useEffect(() => {
+        const upload = async () => {
+            const uploadedFile = (url: string) => {
+                console.log('Uploaded file URL:', url);
+                setFileUrl(url)
+            };
+
+            const handleError = (error: Error) => {
+                console.error(`Error uploading file ${file.name}: ${error.message}`);
+            }
+
+            try {
+                const url = await uploadFileToS3(file);
+                if (url !== null) {
+                    uploadedFile(url);
+                }
+            } catch (error: any) { // Typed as any, or use a more specific type if known
+                handleError(error);
+            }
+        };
+
+        upload();
+    }, [file]);
+
+    useImperativeHandle(ref, () => ({
+        handleSubmitEvent() {
+            console.log('Submit file URL:', fileUrl);
+        }
+      }));
+
+    return <StyledFileItem>{file.name}</StyledFileItem>;
+})
--- frontend/src/FileList.tsx
+++ frontend/src/FileList.tsx
@@ -1,47 +1,9 @@
 import styled from 'styled-components';
-import { uploadFileToS3 } from './file_utils';
-import React, { useEffect } from 'react';
-
+import { FileItem, FileItemHandle } from './FileItem';
 const FileListWrapper = styled.ul`
     list-style-type: none;
     padding: 0;
 `;
-
-const StyledFileItem = styled.li`
-    margin: 10px 0;
-`;
-
-interface FileItemProps {
-    file: File;
-}
-
-function FileItem({ file }: FileItemProps) {
-    useEffect(() => {
-        const upload = async () => {
-            const uploadedFile = (url: string) => {
-                console.log('Uploaded file URL:', url);
-            };
-
-            const handleError = (error: Error) => {
-                console.error(`Error uploading file ${file.name}: ${error.message}`);
-            }
-
-            try {
-                const url = await uploadFileToS3(file);
-                if (url !== null) {
-                    uploadedFile(url);
-                }
-            } catch (error) {
-                handleError(error);
-            }
-        };
-
-        upload();
-    }, [file]);
-
-    return <StyledFileItem>{file.name}</StyledFileItem>; // Assuming you want to display the file name
-}
-
 interface FileListProps {
     files: File[];
 }
